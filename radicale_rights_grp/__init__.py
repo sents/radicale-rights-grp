1d7af357922169c2fe5911f60919172ffa6230a9
Finn Krein <finnkrein@googlemail.com>
2020-02-28 15:16:48 +0100

    Use group_prefix when checking if group exists.

--- radicale_rights_grp/__init__.py
+++ radicale_rights_grp/__init__.py
@@ -14,8 +14,6 @@ class Rights(rights.BaseRights):
         )
 
     def user_in_group(self, user, group):
-        if self.group_prefix is not None:
-            group = self.group_prefix + group
         if user in grp.getgrnam(group).gr_mem:
             return True
         else:
@@ -36,18 +34,23 @@ class Rights(rights.BaseRights):
         sane_path = sanitize_path(path)
         sane_path = sane_path.lstrip("/")
         pathowner, subpath = sane_path.split("/", maxsplit=1)
+        fpathowner = (
+            (self.group_prefix + pathowner)
+            if self.group_prefix is not None
+            else pathowner
+        )
         if user == pathowner:
             self.logger.debug("User %r is pathowner. Access granted.", user)
             return True
-        elif pathowner in self.groupnames:
+        elif fpathowner in self.groupnames:
             # Check if pathowner is group of user
-            in_group = self.user_in_group(user, pathowner)
+            in_group = self.user_in_group(user, fpathowner)
             if in_group is True:
                 self.logger.debug(
                     "User %r is in pathowner group %r. Access granted.", user, pathowner
                 )
                 return True
-        self.logger.debug("Access to path %r is not granted to user %r.",
-                          pathowner,
-                          user)
+        self.logger.debug(
+            "Access to path %r is not granted to user %r.", pathowner, user
+        )
         return False
